8de7da6880ce907a24970210f0a09a8b37a5f98e
7anmayog <tanmaysahu196@gmail.com>
2025-09-20 23:50:55 +0530

    printed the pattern 3 sucessfully

--- pattern_3.cpp
+++ pattern_3.cpp
@@ -14,8 +14,8 @@
 using namespace std;
 
 void print3(int n) {
-    for (int i = 0;i<=n;i++) {
-        for (int j = 0;j<=i;j++) {
+    for (int i = 1;i<=n;i++) {
+        for (int j = 1;j<=i;j++) {
             cout << j << " ";
         }
         cout << endl;
