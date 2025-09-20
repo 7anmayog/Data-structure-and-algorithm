1801f9c3bad026e7c53e8b0ba607c4a8961cba9b
7anmayog <tanmaysahu196@gmail.com>
2025-09-20 23:46:53 +0530

    changed the output pattern a little

--- pattern_1.cpp
+++ pattern_1.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 void print1(int n) {
-    for (int i = 0;i<=n;i++) {
+    for (int i = 1;i<=n;i++) {
         for (int j = 0;j<=n;j++) {
             cout << "* ";
         }
