d7db0e7d5ad73505031891defa41ed73ee79d3fa
7anmayog <tanmaysahu196@gmail.com>
2025-09-20 23:49:49 +0530

    tanmay

--- pattern_1.cpp
+++ pattern_1.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 void print1(int n) {
     for (int i = 1;i<=n;i++) {
-        for (int j = 0;j<=n;j++) {
+        for (int j = 1;j<=n;j++) {
             cout << "* ";
         }
         cout << endl;
