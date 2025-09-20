392171db8101509b530a8ec3e0a2d49a969cc4f9
7anmayog <tanmaysahu196@gmail.com>
2025-09-20 23:37:32 +0530

    changed something

--- pattern_4.cpp
+++ pattern_4.cpp
@@ -1,11 +1,11 @@
 /*print the following pattern
 
 
-1
-1 2
-1 2 3
-1 2 3 4
-1 2 3 4 5
+*
+* * 
+* * * 
+* * * * 
+* * * * *
 
 
 */
@@ -13,10 +13,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print3(int n) {
+void print2(int n) {
     for (int i = 0;i<=n;i++) {
         for (int j = 0;j<=i;j++) {
-            cout << j << " ";
+            cout << "* ";
         }
         cout << endl;
     }
@@ -25,7 +25,7 @@ void print3(int n) {
 int main () {
     int n;
     cin >> n;
-    print3(n);
+    print2(n);
 }
 
 
