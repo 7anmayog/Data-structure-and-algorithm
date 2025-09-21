6b5d7c30cd44cb575d149a2108fef9efec1f9fbd
7anmayog <tanmaysahu196@gmail.com>
2025-09-21 23:20:51 +0530

    trying to correct pattern 5

--- pattern_5.cpp
+++ pattern_5.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 void print5(int n) {
     for (int i = 1;i<=n;i++) {
-        for (int j = 1;j<n-i+1;j++) {
+        for (int j = 1;j<=n-i+1;j++) {
             cout << "* ";
         }
         cout << endl;
